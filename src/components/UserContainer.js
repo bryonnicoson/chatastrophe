23c3836b2f9456add5dfa272bcde4c444238ec66
Bryon Nicoson <bryon.nicoson@gmail.com>
2017-11-21 22:12:32 -0600

    logo fix

--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Header from './Header';
 import { Link } from 'react-router-dom';
+import Header from './Header';
 
 export default class UserContainer extends Component {
 
