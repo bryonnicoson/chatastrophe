3974f0ae757aa69b3ca76ee12b09da1a27d24f27
Bryon Nicoson <bryon.nicoson@gmail.com>
2017-11-22 18:49:00 -0600

    push permissions

--- src/resources/NotificationResource.js
+++ src/resources/NotificationResource.js
@@ -1,5 +1,17 @@
 export default class NotificationResource {
 	constructor(messaging) {
-		console.log("Instantiated!");
+		this.messaging = messaging;
+		try {
+			this.messaging
+				.requestPermission()
+				.then(res => {
+					console.log('Permission granted');
+				})
+				.catch(err => {
+					console.log('no access', err);
+				});
+		} catch(err) {
+			console.log('No notification support', err);
+		}
 	}
 }
